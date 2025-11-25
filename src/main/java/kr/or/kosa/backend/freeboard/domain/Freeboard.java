3a7e92592d2ff24051a09cdc05e9dbcd0f3f43b0
unknown <blackteatime4@naver.com>
2025-11-25 19:01:16 +0900

    feat: 본문 내용 구분 주석 추가

--- src/main/java/kr/or/kosa/backend/freeboard/domain/Freeboard.java
+++ src/main/java/kr/or/kosa/backend/freeboard/domain/Freeboard.java
@@ -8,8 +8,8 @@ public class Freeboard {
     private Long freeboardId;
     private Long userId;
     private String freeboardTitle;
-    private String freeboardContent;
-    private String freeboardPlainText;
+    private String freeboardContent;     // HTML 형식의 전체 콘텐츠 <h1>제목</h1>
+    private String freeboardPlainText;   // HTML 태그를 제거한 순수 텍스트, 검색, 미리보기에 사용
     private Long freeboardClick;
     private String freeboardImage;
     private String freeboardRepresentImage;
