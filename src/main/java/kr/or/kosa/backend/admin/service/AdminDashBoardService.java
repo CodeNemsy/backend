49a2c928f9cda3ad95fadc1905df01e3d7ffc7d8
mok <99533961+parcPrive@users.noreply.github.com>
2025-12-10 18:43:41 +0900

    어드민_대시보드_작업_3

--- src/main/java/kr/or/kosa/backend/admin/service/AdminDashBoardService.java
+++ src/main/java/kr/or/kosa/backend/admin/service/AdminDashBoardService.java
@@ -1,5 +1,7 @@
 package kr.or.kosa.backend.admin.service;
 
+import kr.or.kosa.backend.admin.dto.response.AdminDashBoardResponseDto;
+
 public interface AdminDashBoardService {
-    int dashBoards(); // 여기서 createdat으로 조절?
+    AdminDashBoardResponseDto dashBoards(); // 여기서 createdat으로 조절?
 }
